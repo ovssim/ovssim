7364806f4b3cf5842bc5910329c20cb3fffb5c05
Ohveeff <aingunj@gmail.com>
2026-02-05 21:27:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let caseData;
+
+fetch("data/cases.json")
+  .then(res => res.json())
+  .then(data => {
+    caseData = data.cases[0];
+    document.getElementById("case-container").innerHTML =
+      `<img src="${caseData.image}"><p>$${caseData.price}</p>`;
+  });
+
+document.getElementById("open-btn").addEventListener("click", () => {
+  const item = openCase(caseData.items);
+
+  document.getElementById("result").innerHTML = `
+    <h2 class="${item.rarity}">${item.name}</h2>
+    <img src="${item.image}">
+    <p>$${item.price}</p>
+  `;
+});
+
+function openCase(items) {
+  const total = items.reduce((s, i) => s + i.weight, 0);
+  let roll = Math.random() * total;
+
+  for (let item of items) {
+    if (roll < item.weight) return item;
+    roll -= item.weight;
+  }
+}
 
